5cfccbed8e38d0b2cb375de5e1742da055c7d392
kaizen <belebraz16@gmail.com>
2025-09-24 07:47:35 -0300

    :sparkles: feat: add CommandsStrategy

---
+++ src/main/java/br/com/pastelaria/domain/context/CommandsStrategy.java
@@ -0,0 +1,23 @@
+package br.com.pastelaria.domain.context;
+
+import br.com.pastelaria.domain.commands.*;
+import br.com.pastelaria.domain.interfaces.iCommand;
+
+import java.util.HashMap;
+
+public class CommandsStrategy {
+    private final HashMap<String, iCommand> commands;
+
+    public CommandsStrategy() {
+        this.commands = new HashMap<>();
+        this.commands.put("!clear", new Clear());
+        this.commands.put("!write", new Write());
+        this.commands.put("!scheduler", new Scheduler());
+        this.commands.put("!gossip", new Gossip());
+        this.commands.put("!done", new Done());
+    }
+
+    public iCommand getCommand(String command) {
+        return this.commands.getOrDefault(command, new NoCommand());
+    }
+}
