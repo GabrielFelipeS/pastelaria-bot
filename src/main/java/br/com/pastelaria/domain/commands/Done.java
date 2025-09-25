ebd8ec1a0484195bffb3c84ac1ce3a99b34fa145
kaizen <belebraz16@gmail.com>
2025-09-25 19:48:53 -0300

    :sparkles: feat: add Done

--- src/main/java/br/com/pastelaria/domain/commands/Done.java
+++ src/main/java/br/com/pastelaria/domain/commands/Done.java
@@ -1,18 +1,13 @@
 package br.com.pastelaria.domain.commands;
 
 import br.com.pastelaria.domain.interfaces.iCommand;
-import br.com.pastelaria.domain.service.EventBot;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import br.com.pastelaria.domain.interfaces.iEventBot;
+import br.com.pastelaria.domain.utils.FileUtils;
 
 
 public class Done implements iCommand {
-    private static final String already_done_image = "https://media.discordapp.net/attachments/1420084043847827676/1420109496612819105/deu.png?ex=68d43388&is=68d2e208&hm=6094bf5ec7833cba1d77b3029d64b4371cafdeeb6cf82a5b801af6bb4e89ed3a&=&format=webp&quality=lossless";
     @Override
-    public void execute(EventBot event) {
-        event.deleteCommandMessage();
-
-        MessageChannel channel = event.getChannel();
-
-        channel.sendMessage(already_done_image).queue();
+    public void execute(iEventBot event) {
+        event.reply("", FileUtils.getFile("assets/deu.png"));
     }
 }
