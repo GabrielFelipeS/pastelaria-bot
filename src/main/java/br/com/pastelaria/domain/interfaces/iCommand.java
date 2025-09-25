cf65cf89336f3f61d576b341608652aa0f0b6504
kaizen <belebraz16@gmail.com>
2025-09-25 19:51:34 -0300

    :recycle: refactor: use iEventBot interface instead of EventBot class

--- src/main/java/br/com/pastelaria/domain/interfaces/iCommand.java
+++ src/main/java/br/com/pastelaria/domain/interfaces/iCommand.java
@@ -1,9 +1,8 @@
 package br.com.pastelaria.domain.interfaces;
 
 
-import br.com.pastelaria.domain.service.EventBot;
 
 @FunctionalInterface
 public interface iCommand {
-    void execute(EventBot event);
+    void execute(iEventBot event);
 }
