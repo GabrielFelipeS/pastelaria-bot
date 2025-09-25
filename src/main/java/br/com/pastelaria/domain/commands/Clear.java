45cea1d08a5684c22720568793865a29eeb1589a
kaizen <belebraz16@gmail.com>
2025-09-25 19:48:38 -0300

    :sparkles: feat: add Clear

--- src/main/java/br/com/pastelaria/domain/commands/Clear.java
+++ src/main/java/br/com/pastelaria/domain/commands/Clear.java
@@ -1,24 +1,30 @@
 package br.com.pastelaria.domain.commands;
 
 import br.com.pastelaria.domain.interfaces.iCommand;
-import br.com.pastelaria.domain.service.EventBot;
+import br.com.pastelaria.domain.interfaces.iEventBot;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
 import java.util.concurrent.TimeUnit;
 
 public class Clear implements iCommand {
     @Override
-    public void execute(EventBot event) {
-        event.deleteCommandMessage();
-
+    public void execute(iEventBot event) {
         MessageChannel channel = event.getChannel();
 
-        int amount = event.hasParams()? event.getParamAsInt(1) : 100;
+        var option = event.getEvent().getOption("quantity");
+
+        int amount = option != null? option.getAsInt() : 10;
+
+        if(amount < 1 || amount > 100) {
+            event.reply("⚠\uFE0F Você tem que passar um valor entre 1 e 100");
+            return;
+        }
 
         channel.getHistory().retrievePast(amount).queue(messages -> {
             channel.purgeMessages(messages);
-            channel.sendMessage("✅ Apaguei " + messages.size() + " mensagens!").queue(msg -> {
-                msg.delete().queueAfter(5, TimeUnit.SECONDS);
+
+            event.replyWithQueue("✅ Apaguei " + messages.size() + " mensagens!").queue(msg -> {
+                msg.deleteOriginal().queueAfter(5, TimeUnit.SECONDS);
             });
         });
     }
